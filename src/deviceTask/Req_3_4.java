4508857c46dbfacb691772ce6409a106e6a89181
Zulkii <zulkar213@gmail.com>
2022-08-04 22:48:39 +0200

    class created

---
+++ src/deviceTask/Req_3_4.java
@@ -0,0 +1,18 @@
+package deviceTask;
+
+public class Phone extends Device {
+
+
+
+}
+/*
+ Create a subclass of Device named Phone:
+
+        Extra Methods:
+        call(long phoneNum), text(long phoneNum)
+
+        3. Create a subclass of Device named Computer:
+
+        Add the extra methods if needed
+
+ */
